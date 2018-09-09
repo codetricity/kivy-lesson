1ceef361f69af6c0038a6300a3b4b44417ae961b
Craig Oda <craig@oppkey.com>
2018-09-09 14:41:53 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,33 @@
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.properties import ObjectProperty
+
+
+class ThetaTesterApp(App):
+    # def build(self):
+    #     greeting = "hello, world"
+    #     return Button(text=greeting)
+    pass
+
+
+class FirstScreen(GridLayout):
+
+    input_box = ObjectProperty
+
+    def printIt(self):
+        output_string = "the 1st button was pressed"
+        print(output_string)
+        self.button_output.text = output_string
+
+    def secondButton(self):
+        print("the 2nd button was pressed")
+        print(self.button_output.text)
+        self.button_output.text = (
+            "Button 2 pressed. " +
+            "Input box contains: " + self.input.text)
+        print(self.input.text)
+
+
+if __name__ == '__main__':
+    ThetaTesterApp().run()
